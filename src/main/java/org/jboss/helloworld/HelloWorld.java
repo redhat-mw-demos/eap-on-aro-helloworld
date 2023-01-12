6cf06c475081b215f6e562dc01a463bfea071bb6
Philip Hayes <philip@deewhy.ie>
2023-01-12 15:32:24 -0500

    Added kube api

--- src/main/java/org/jboss/helloworld/HelloWorld.java
+++ src/main/java/org/jboss/helloworld/HelloWorld.java
@@ -30,8 +30,11 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import io.kubernetes.client.openapi.ApiClient;
+import io.kubernetes.client.openapi.ApiException;
+import io.kubernetes.client.openapi.Configuration;
 import io.kubernetes.client.openapi.apis.CoreV1Api;
-import io.kubernetes.client.openapi.models.V1NodeList;
+import io.kubernetes.client.openapi.models.V1Pod;
+import io.kubernetes.client.openapi.models.V1PodList;
 import io.kubernetes.client.util.Config;
 
 /**
@@ -49,12 +52,14 @@ public class HelloWorld {
     @Path("/javadetails")
     @Produces(MediaType.APPLICATION_JSON)
     public Map<String, String> geJavaDetails() throws Exception  {
-        ApiClient client  = Config.defaultClient();
-        CoreV1Api api = new CoreV1Api(client);
-        V1NodeList nodeList = api.listNode(null, null, null, null, null, null, null, null, 10, false);
-        nodeList.getItems()
-          .stream()
-          .forEach((node) -> System.out.println(node.getMetadata()));
+        ApiClient client = Config.defaultClient();
+        Configuration.setDefaultApiClient(client);
+
+        CoreV1Api api = new CoreV1Api();
+        V1PodList list = api.listPodForAllNamespaces(null, null, null, null, null, null, null, null, null, null);
+        for (V1Pod item : list.getItems()) {
+            System.out.println(item.getMetadata().getName());
+        }
 
         Map<String, String> dets = new HashMap<String, String>();
         dets.put("Vendor", System.getProperty("java.vendor"));
